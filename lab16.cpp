181462fa1c7e0cc1b5b7819cc72efaccc6020896
jillian hillstrom <jhillstr@umich.edu>
2024-11-14 19:12:57 -0500

    short code

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,10 @@
 // Code to implement the gcf function goes here
+
+int gcf(int a, int b) {
+    // Base case: if b is 0, the GCF is a
+    if (b == 0) {
+        return a;
+    }
+    // Recursive case: GCF(a, b) = GCF(b, a % b)
+    return gcf(b, a % b);
+}
